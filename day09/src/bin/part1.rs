b42bc6d80b0673da2778b8f63c3bf7303c5063d5
bogdan <bogdanvcucu@gmail.com>
2023-12-09 17:25:59 +0200

    day 9 part 1

---
+++ day09/src/bin/part1.rs
@@ -0,0 +1,30 @@
+use color_eyre::eyre::{eyre, Ok, Result};
+use day09::{calculate_next, read_input};
+
+pub fn main() -> Result<()> {
+    let contents = read_input("./src/input.txt")?;
+    let lines = contents.lines();
+
+    let sequences: Result<Vec<Vec<isize>>> = lines
+        .map(|s| {
+            let r: Result<Vec<isize>> = s
+                .split_whitespace()
+                .map(|num_str| {
+                    num_str
+                        .parse::<isize>()
+                        .map_err(|_| eyre!("could not parse input {}", num_str))
+                })
+                .collect();
+            r
+        })
+        .collect();
+    let sequences = sequences?;
+
+    let result: Result<isize> = sequences
+        .iter()
+        .map(|sequence| calculate_next(sequence))
+        .sum();
+    println!("total is {}", result?);
+
+    Ok(())
+}
---
+++ day09/src/lib.rs
@@ -0,0 +1,31 @@
+use std::io::Read;
+
+use color_eyre::{eyre::eyre, Result};
+
+pub fn read_input(path: &str) -> Result<String> {
+    let mut file = std::fs::File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+pub fn calculate_next(input: &[isize]) -> Result<isize> {
+    if input.iter().all(|elem| *elem == 0) {
+        return Ok(0);
+    }
+    let input_length = input.len();
+    if input_length < 2 {
+        return Err(eyre!("went to the bottom without finding a rule"));
+    }
+    let l1 = input.get(0..input_length - 1).unwrap();
+    let l2 = input.get(1..input_length).unwrap();
+    let r: Vec<isize> = l2
+        .iter()
+        .zip(l1.iter())
+        .map(|(elem2, elem1)| *elem2 - *elem1)
+        .collect();
+    match calculate_next(&r) {
+        Ok(val) => Ok(input.last().unwrap() + val),
+        Err(e) => Err(e),
+    }
+}
