f505824c214e827333d2c5152689f3362eb05ae8
bogdan <bogdanvcucu@gmail.com>
2023-12-09 17:37:47 +0200

    day 9 part 2

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -29,3 +29,23 @@ pub fn calculate_next(input: &[isize]) -> Result<isize> {
         Err(e) => Err(e),
     }
 }
+pub fn calculate_prev(input: &[isize]) -> Result<isize> {
+    if input.iter().all(|elem| *elem == 0) {
+        return Ok(0);
+    }
+    let input_length = input.len();
+    if input_length < 2 {
+        return Err(eyre!("went to the bottom without finding a rule"));
+    }
+    let l1 = input.get(0..input_length - 1).unwrap();
+    let l2 = input.get(1..input_length).unwrap();
+    let r: Vec<isize> = l2
+        .iter()
+        .zip(l1.iter())
+        .map(|(elem2, elem1)| *elem2 - *elem1)
+        .collect();
+    match calculate_prev(&r) {
+        Ok(val) => Ok(input.first().unwrap() - val),
+        Err(e) => Err(e),
+    }
+}
