f0f0d03434fde5d757ee16e5ef01f5d3896a59d4
bogdan <bogdanvcucu@gmail.com>
2023-12-13 06:24:59 +0200

    day 12 part 1

---
+++ day12/src/bin/part1.rs
@@ -0,0 +1,73 @@
+use color_eyre::Result;
+use day12::{
+    generate_constraints, generate_working_spring_sections, parse_input_line, read_input,
+    string_is_valid,
+};
+
+pub fn main() -> Result<()> {
+    let contents = read_input("./src/input.txt")?;
+    let parsed_input: Result<Vec<(&str, Vec<usize>)>> =
+        contents.lines().map(parse_input_line).collect();
+    let parsed_input = parsed_input?;
+
+    let mut valid_string_counter = 0;
+    for line in parsed_input.iter() {
+        let damaged_spring_list = line.1.clone();
+        let damaged_section_count = damaged_spring_list.len();
+
+        let known_sections_generated: Vec<(String, usize)> = damaged_spring_list
+            .iter()
+            .map(|elem| {
+                let r: String = (0..*elem).map(|_| '#').collect();
+                let length = r.len();
+                (r, length)
+            })
+            .collect();
+
+        let mut damaged_sections: Vec<String> = known_sections_generated
+            .iter()
+            .map(|(section, _)| section.to_string())
+            .collect();
+        let damaged_sections_length = known_sections_generated
+            .iter()
+            .map(|(_, length)| *length)
+            .sum::<usize>();
+
+        let (spring_list_length, constraints) = generate_constraints(line.0);
+        let remaining_length = spring_list_length - damaged_sections_length;
+
+        let possible_new_sections_list =
+            match generate_working_spring_sections(remaining_length, damaged_section_count) {
+                Some(val) => val,
+                None => continue,
+            };
+        for mut possible_new_sections in possible_new_sections_list.into_iter() {
+            let new_section_count = possible_new_sections.len();
+
+            if new_section_count >= damaged_section_count {
+                damaged_sections.push("".to_string());
+                let result = possible_new_sections
+                    .iter()
+                    .zip(damaged_sections.iter())
+                    .map(|(fst, lst)| fst.to_string() + lst)
+                    .fold("".to_string(), |acc, elem| acc + elem.as_str());
+
+                let _ = damaged_sections.pop();
+                valid_string_counter += string_is_valid(&result, &constraints) as usize;
+            }
+
+            if new_section_count <= damaged_section_count {
+                possible_new_sections.push("".to_string());
+                let result = damaged_sections
+                    .iter()
+                    .zip(possible_new_sections.iter())
+                    .map(|(fst, lst)| fst.to_string() + lst)
+                    .fold("".to_string(), |acc, elem| acc + elem.as_str());
+
+                valid_string_counter += string_is_valid(&result, &constraints) as usize;
+            }
+        }
+    }
+    println!("{}", valid_string_counter);
+    Ok(())
+}
---
+++ day12/src/lib.rs
@@ -0,0 +1,105 @@
+use std::io::Read;
+
+use color_eyre::{eyre::eyre, Result};
+
+pub fn read_input(path: &str) -> Result<String> {
+    let mut file = std::fs::File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+pub fn parse_input_line(s: &str) -> Result<(&str, Vec<usize>)> {
+    let mut parts = s.split_whitespace();
+    let springs = parts.next().ok_or(eyre!("misshapen input"))?;
+    let damaged_spring_list = parts.next().ok_or(eyre!("misshapen input"));
+    let damaged_spring_list: Result<Vec<usize>> = damaged_spring_list?
+        .split(',')
+        .map(|num_str| {
+            num_str
+                .parse::<usize>()
+                .map_err(|_| eyre!("found something that is not a number in the second section"))
+        })
+        .collect();
+    if parts.next().is_some() {
+        return Err(eyre!("line has more sections than expected"));
+    }
+    Ok((springs, damaged_spring_list?))
+}
+
+pub fn generate_constraints(s: &str) -> (usize, Vec<(usize, char)>) {
+    let length = s.len();
+    let r: Vec<(usize, char)> = s
+        .chars()
+        .enumerate()
+        .filter(|(_, chr)| *chr == '.' || *chr == '#')
+        .collect();
+    (length, r)
+}
+
+pub fn generate_working_spring_sections(
+    spring_count: usize,
+    known_section_count: usize,
+) -> Option<Vec<Vec<String>>> {
+    let mut result = match generate(spring_count, known_section_count - 1) {
+        Some(val) => val,
+        None => Vec::<Vec<usize>>::new(),
+    };
+    result.append(&mut match generate(spring_count, known_section_count) {
+        Some(val) => val,
+        None => Vec::<Vec<usize>>::new(),
+    });
+    result.append(&mut match generate(spring_count, known_section_count + 1) {
+        Some(val) => val,
+        None => Vec::<Vec<usize>>::new(),
+    });
+    if result.is_empty() {
+        return None;
+    }
+    let strings: Vec<Vec<String>> = result
+        .iter()
+        .map(|sections| {
+            sections
+                .iter()
+                .map(|count| (0..*count).map(|_| '.').collect::<String>())
+                .collect::<Vec<String>>()
+        })
+        .collect();
+
+    Some(strings)
+}
+
+fn generate(count: usize, length: usize) -> Option<Vec<Vec<usize>>> {
+    if count == 0 || length == 0 || count < length {
+        return None;
+    }
+    if length == 1 {
+        return Some(vec![vec![count]]);
+    }
+    Some(
+        (1..=count - length + 1)
+            .flat_map(|val| {
+                generate(count - val, length - 1).map(|generated_arrays| {
+                    generated_arrays
+                        .into_iter()
+                        .map(|mut array| {
+                            array.push(val);
+                            array
+                        })
+                        .collect::<Vec<Vec<usize>>>()
+                })
+            })
+            .fold(Vec::<Vec<usize>>::new(), |mut acc, mut elem| {
+                acc.append(&mut elem);
+                acc
+            }),
+    )
+}
+
+pub fn string_is_valid(s: &str, constraints: &[(usize, char)]) -> bool {
+    !s.chars().enumerate().any(|(idx, chr)| {
+        constraints
+            .iter()
+            .any(|(idx_c, chr_c)| idx == *idx_c && chr != *chr_c)
+    })
+}
